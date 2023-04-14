b83be292eba7430a0e1b01201d7424cf2818b948
Ethan Fang <ethan.fang@gmail.com>
2023-04-15 01:08:41 +1000

    Add Todo component

--- app/app.jsx
+++ app/app.jsx
@@ -1,13 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require("react-router");
-
+var Todo = require("Todo");
 
 $(document).foundation();
 
 require("style!css!sass!applicationStyles")
 
 ReactDOM.render(
-  <p>boilerplate</p>,
+  <Todo/>,
   document.getElementById('app')
 );

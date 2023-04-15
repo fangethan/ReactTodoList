fceaa1f163d37ebb66ecf4a08965cf399120eaa1
Ethan Fang <ethan.fang@gmail.com>
2023-04-16 02:35:51 +1000

    Remove unnecessary comments in todo.test

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -12,13 +12,4 @@ describe("Todo", () => {
         expect(Todo).toExist();
     });
 
-    // describe("render", () => {
-    //     it("should render clock to output", () => {
-    //         var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62} />)
-    //         var $el = $(ReactDOM.findDOMNode(clock));
-    //         var actualText = $el.find(".clock-text").text();
-    //         expect(actualText).toBe("01:02")
-    //     })
-    // });
-
 });

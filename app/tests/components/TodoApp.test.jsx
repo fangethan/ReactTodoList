f760347d12dc827397b831776af718883784f97a
Ethan Fang <ethan.fang@gmail.com>
2023-04-19 00:18:35 +1000

    Create test case should add todo to the todos state on handleAddTodoItem

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -11,4 +11,16 @@ describe("TodoApp", () => {
     it("TodoApp should exist", () => {
         expect(TodoApp).toExist();
     });
+
+    it("should add todo to the todos state on handleAddTodoItem", () => {
+        var todoText = "Check email";
+        var todoApp = TestUtils.renderIntoDocument(<TodoApp />)
+        
+        todoApp.setState({todos: []});
+        todoApp.handleAddTodoItem(todoText);
+
+        expect(todoApp.state.todos[0].text).toBe(todoText)
+    });
+    
+
 });

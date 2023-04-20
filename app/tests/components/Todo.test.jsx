c8ba3dffd5ad5c3d78d4219e25da847217c139c2
Ethan Fang <ethan.fang@gmail.com>
2023-04-20 14:04:43 +1000

    Create test case should call onToggle prop with id on clicked

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -12,4 +12,20 @@ describe("Todo", () => {
         expect(Todo).toExist();
     });
 
+    it("should call onToggle prop with id on clicked", () => {
+        var todoData = {
+            id: 11,
+            text: "Walk the dog",
+            completed: false,
+        }
+        var spy = expect.createSpy();
+        var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}/>);
+        
+        var $el = $(ReactDOM.findDOMNode(todo))
+        TestUtils.Simulate.click($el[0])
+
+        expect(spy).toHaveBeenCalledWith(11)
+        
+    });
+
 });
